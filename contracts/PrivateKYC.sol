a35db9630c2cb1c76f9db2561262aaadcb28c3de
Yloseff6 <160427156+Yloseff6@users.noreply.github.com>
2025-08-24 13:06:18 +0300

    Update PrivateKYC.sol

--- contracts/PrivateKYC.sol
+++ contracts/PrivateKYC.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.20;
 contract PrivateKYC {
     mapping(address => bytes) private proofs;
 
+    event ProofSubmitted(address indexed user);
+
     function submitProof(bytes calldata encryptedProof) external {
+        require(encryptedProof.length > 0, "Proof cannot be empty");
         proofs[msg.sender] = encryptedProof;
+        emit ProofSubmitted(msg.sender);
     }
 
     function hasProof(address user) external view returns (bool) {
